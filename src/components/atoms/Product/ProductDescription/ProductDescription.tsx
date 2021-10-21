9f49595be0b6eab1252ea8bafa06ed489fd8d609
Andy <ceandyemmanuel@gmail.com>
2021-10-20 22:59:08 -0500

    Add component description and component image

---
+++ src/components/atoms/Product/ProductDescription/ProductDescription.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+type Props = {
+	name: string
+	cover: string
+	price: number
+}
+
+const ProductDescription = ({ cover, name, price }: Props) => (
+	<div className="mt-4 flex justify-between">
+		<div>
+			<h3 className="text-sm text-gray-700">
+				<a href={cover}>
+					<span aria-hidden="true" className="absolute inset-0" />
+					{name}
+				</a>
+			</h3>
+		</div>
+		<p className="text-sm font-medium text-gray-900">{price}</p>
+	</div>
+)
+
+export default ProductDescription
---
+++ src/components/atoms/Product/ProductImage/ProductImage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+	image_url: string
+}
+
+const ProductImage = ({ image_url }: Props) => (
+	<div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+		<img
+			src={image_url}
+			className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+		/>
+	</div>
+)
+
+export default ProductImage
