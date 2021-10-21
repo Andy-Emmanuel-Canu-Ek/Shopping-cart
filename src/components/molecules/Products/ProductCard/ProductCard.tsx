8bfdf028f75489b69a6e473cc6708908a459a976
Andy <ceandyemmanuel@gmail.com>
2021-10-20 23:00:33 -0500

    Add product card component

---
+++ src/components/molecules/Products/ProductCard/ProductCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Product } from 'api/Products/declaration'
+import ProductImage from 'components/atoms/Product/ProductImage'
+import ProductDescription from 'components/atoms/Product/ProductDescription'
+
+type Props = {
+	product: Product
+}
+
+const ProductCard = ({ product }: Props) => (
+	<div key={product.id} className="group relative">
+		<ProductImage image_url={product.cover} />
+		<ProductDescription {...product} />
+	</div>
+)
+
+export default ProductCard
