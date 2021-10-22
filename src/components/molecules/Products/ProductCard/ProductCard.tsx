db56a670b65e8002a5ac7287f6db3e2e0b309f0c
Andy <ceandyemmanuel@gmail.com>
2021-10-21 22:03:38 -0500

    Add counter input into product card

--- src/components/molecules/Products/ProductCard/ProductCard.tsx
+++ src/components/molecules/Products/ProductCard/ProductCard.tsx
@@ -1,19 +1,25 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { Product } from 'api/Products/declaration'
 import ProductImage from 'components/atoms/Product/ProductImage'
 import ProductDescription from 'components/atoms/Product/ProductDescription'
 import AddCartButton from 'components/atoms/AddCartButton'
+import CounterInput from 'components/atoms/CounterInput'
 
 type Props = {
 	product: Product
 }
 
-const ProductCard = ({ product }: Props): ReactElement => (
-	<a className="group">
-		<ProductImage image_url={product.cover} />
-		<ProductDescription {...product} />
-		<AddCartButton />
-	</a>
-)
+const ProductCard = ({ product }: Props): ReactElement => {
+	const [counter, setCounter] = useState(0)
+
+	return (
+		<a className="group">
+			<ProductImage image_url={product.cover} />
+			<ProductDescription {...product} />
+			<CounterInput value={counter} setCounter={setCounter} />
+			<AddCartButton />
+		</a>
+	)
+}
 
 export default ProductCard
