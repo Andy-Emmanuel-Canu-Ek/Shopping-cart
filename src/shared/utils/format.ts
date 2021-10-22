971812681a0df42b46111dfecc3a02348637addb
Andy <ceandyemmanuel@gmail.com>
2021-10-21 19:30:47 -0500

    Add product format

---
+++ src/shared/utils/format.ts
@@ -0,0 +1,10 @@
+import { Product } from 'api/Products/declaration'
+
+const MAX_STRING_LENGTH = 50;
+export const formatProduct = (products: Product[]) =>
+	products.map(({ name, ...rest }: Product) => ({
+		name: name.length > MAX_STRING_LENGTH 
+                ? `${name.slice(0, MAX_STRING_LENGTH)}...` 
+				: name.slice(0, MAX_STRING_LENGTH),
+		...rest,
+	}))
