2f7d0e73d36829806cc2a2de3320e3fde900963f
Andy <ceandyemmanuel@gmail.com>
2021-10-24 01:11:59 -0500

    Change loading spinner color and border size

--- src/components/atoms/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/atoms/LoadingSpinner/LoadingSpinner.tsx
@@ -5,7 +5,7 @@ const LoadingSpinner = (): ReactElement => (
 		<div
 			className="
             animate-spin rounded-full 
-            h-32 w-32 border-b-2 border-gray-900"
+            h-32 w-32 border-b-4 border-purple-900"
 		/>
 	</div>
 )
